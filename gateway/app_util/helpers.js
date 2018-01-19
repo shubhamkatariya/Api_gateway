cfa4921b52728f7c69f69ab4580d190b7366cf86
shubhamkatariya <shubham.katariya@newput.com>
2018-01-19 10:26:40 +0530

    Removed logs

--- gateway/app_util/helpers.js
+++ gateway/app_util/helpers.js
@@ -21,9 +21,6 @@ module.exports.convertToSlug = function(txt) {
 }
 
 module.exports.verifyAuthToken = function(req, res, next) {
-  console.log(req.session.token);
-  console.log(req.session);
-  console.log(req.headers['token']);
   if(!req.headers['token']){
     return response.errorResponse(req, res, appException.VALIDATION_EXCEPTION(499, "Request does not contain token"), null)
   }
@@ -31,7 +28,6 @@ module.exports.verifyAuthToken = function(req, res, next) {
     return response.errorResponse(req, res, appException.VALIDATION_EXCEPTION(440, "Your session has been expired"), null)
   }
   else if (req.session.token != req.headers['token']){
-    console.log("/////////");
     return response.errorResponse(req, res, appException.VALIDATION_EXCEPTION(498, "Invalid token"), null)
   }
   else{
